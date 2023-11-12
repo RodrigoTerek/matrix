9f35c1f2ea1a208b538a6c9ea32953315ad116e0
Rodrigo Terek <r.terekrosa@elsevier.com>
2023-11-12 20:14:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import chalk from 'chalk';
+
+const SHADES = ['#00FF00', '#00E600', '#00CC00', '#00B300', '#009900', '#007F00', '#006600', '#004C00', '#003300', '#001A00'];
+
+const sleep = (milisseconds = 1000) =>
+    new Promise((resolve) => setTimeout(resolve, milisseconds))
+
+const getRandomChar = () => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    return alphabet[randomIndex];
+}
+
+const rows = process.stdout.rows;
+const columns = process.stdout.columns;
+
+const stream = [];
+for (let i = 0; i < columns; i++)
+    stream[i] = Math.floor(Math.random() * rows);
+
+const run = async () => {
+    while (true) {
+        console.clear()
+
+        for (let i = 0; i < rows; i++) {
+            let line = ""
+            for (let j = 0; j < columns; j++) {
+                const shade = stream[j] - i
+                if (shade >= 0 && shade < 10)
+                    line += chalk.hex(SHADES[shade])(getRandomChar())
+                else if (stream[j] < 10 && rows - i < 10 - stream[j])
+                    line += chalk.hex(SHADES[stream[j]])(getRandomChar())
+                else
+                    line += ' '
+            }
+
+            console.log(line);
+        }
+
+        for (let i = 0; i < stream.length; i++)
+            stream[i] = (stream[i] + 1) % rows;
+
+        await sleep(100)
+    }
+}
+
+run()
